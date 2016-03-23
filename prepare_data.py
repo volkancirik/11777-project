9e6eb614e0f7b193e03634edb19baece5675e1c7
Volkan Cirik <vcirik@atlas4.multicomp.cs.cmu.edu>
2016-03-23 01:33:40 -0400

    prepate_data is updated

--- prepare_data.py
+++ prepare_data.py
@@ -17,145 +17,140 @@ def open_file(fname):
 		quit(0)
 	return f
 
-def process_target(fname, word_idx, tokenize = False, max_len = -1):
+def process_target(fname, word_idx, tokenize = False, max_len = -1, use_hierarchical = True):
 
 	in_file = open_file(fname)
 	V = len(word_idx)
-	unk = 0.0
-	ntok = 0.0
+	dim = int(pow(V,0.25)) + 1
 
-	S = [line.lower().strip().split() + [EOS] for line in in_file]
+	S = [line.strip().split() + [EOS] for line in in_file]
 	N = len(S)
 
-	Y = np.zeros((N,max_len,V), dtype=np.bool)
+	if use_hierarchical:
+		print >> sys.stderr, "using hierarchical softmax with %d dimensional vectors " % (dim)
+		Y_1 = np.zeros((N,max_len, dim), dtype=np.bool)
+		Y_2 = np.zeros((N,max_len, dim), dtype=np.bool)
+		Y_3 = np.zeros((N,max_len, dim), dtype=np.bool)
+		Y_4 = np.zeros((N,max_len, dim), dtype=np.bool)
+	else:
+		Y = np.zeros((N,max_len,V), dtype=np.bool)
 	for i,s in enumerate(S):
-		ntok += len(s)
 		for j,tok in enumerate(s):
-			try:
-				idx = word_idx[tok]
-			except:
-				unk += 1
-				idx = word_idx[UNK]
-				pass
-			Y[i,j,idx] = 1
-	print >> sys.stderr, "UNK rate for %s is %f" % (fname, unk/ntok)
+			idx = word_idx[tok]
+
+			if use_hierarchical:
+				idx_1 = idx / (dim**3)
+				idx_2 = (idx - (dim**3)*idx_1) / (dim**2)
+				idx_3 = (idx - (dim**3)*idx_1 - (dim**2)*idx_2) / (dim)
+				idx_4 = idx % (dim)
+
+				Y_1[i, j, idx_1 ] = 1
+				Y_2[i, j, idx_2 ] = 1
+				Y_3[i, j, idx_3 ] = 1
+				Y_4[i, j, idx_4 ] = 1
+
+			else:
+				Y[i, j, idx ] = 1
+	if use_hierarchical:
+		return Y_1,Y_2,Y_3,Y_4
 	return Y
 
-def process_source(fname, v_map, dim, mini_batch = False, max_len = -1):
+def process_source(fname, word_idx_en, max_len = -1):
 
 	in_file = open_file(fname)
 	S = [line for line in in_file]
 	N = len(S)
+	X = np.zeros((N,max_len), dtype = 'int64')
 
-	unk = 0.0
-	ntok = 0.0
-
-	if mini_batch:
-		X = np.zeros((N,max_len,dim), dtype = theano.config.floatX)
-	else:
-		X = []
-	for idx,line in enumerate(S):
-		s = nltk.word_tokenize(line.lower().strip())
-		x = np.zeros((1,len(s), dim), dtype=theano.config.floatX)
-		ntok += len(s)
+	length = []
+	for i,line in enumerate(S):
+		s = line.strip().split() + [EOS]
+		pad = max_len - len(s) #
 		for j,tok in enumerate(s):
-			try:
-				v = v_map[tok]
-			except:
-				unk +=1
-				v = v_map[UNK]
-				pass
-			if mini_batch:
-				X[idx,j,:] = list(v)
-			else:
-				x[0,j,:] = list(v)
-		if not mini_batch:
-			X += [x]
-	print >> sys.stderr, "UNK rate for %s is %f" % (fname, unk/ntok)
-	return X
+			X[i,pad + j] = word_idx_en[tok] #
+		length += [len(s)]
+	return X, length
 
-def process_image(fname):
+def process_image(fname, repeat = False):
 	f = open_file(fname)
 	package = pickle.load(f)
 	if len(package['features'].shape) == 4:
-		return package['features'].reshape(package['features'].shape[0],package['features'].shape[1],-1)
+		feats =  package['features'].reshape(package['features'].shape[0],package['features'].shape[1],-1)
 	else:
-		return package['features']
+		feats =  package['features']
+	if repeat:
+		return np.repeat(feats,5,axis = 0)
+	return feats
 
-def get_max_len(f_list, tokenize = False):
+def get_max_len(f_list):
 	sen_len = []
 	for f in f_list:
-		if tokenize:
-			sen_len += [ len(nltk.word_tokenize(line.lower().strip())) for line in f]
-		else:
-			sen_len += [ len(line.strip().split()) for line in f]
+		sen_len += [ len(line.strip().split()) + 1 for line in f]
 	return np.max(sen_len)
 
-def prepare_train(path_prefix = '../data/', train_source = 'train.en', train_target = 'train.de', val_source = 'val.en', val_target = 'val.de', wvec = '../embeddings/word2vec.pkl',TRESHOLD = 1, train_img = 'TRAIN.vgg19.fc7.pkl', val_img = 'VAL.vgg19.fc7.pkl', mini_batch = False):
+def get_dicts(f_list):
 
-	tr_t = open_file(path_prefix + train_target)
-	val_t = open_file(path_prefix + val_target)
+	vocab = defaultdict(int)
+	for f_name in f_list:
+		f = open_file(f_name)
+		for line in f:
+			for tok in line.strip().split():
+				vocab[tok] +=1
 
-	tr_s = open_file(path_prefix + train_source)
-	val_s = open_file(path_prefix + val_source)
+	vocab[UNK] = 1
+	vocab[EOS] = 1
+	word_idx = dict((c, i) for i, c in enumerate(vocab))
+	idx_word = dict((i, c) for i, c in enumerate(vocab))
 
-	try:
-		print >> sys.stderr, "loading word vectors..."
-		v_map = pickle.load(gzip.open(wvec, "rb"))
-		dim = len(list(v_map[UNK]))
-		print >> sys.stderr, "%d dim word vectors are loaded.." % (dim)
-	except:
-		print >> sys.stderr, "word embedding file %s cannot be read." % (wvec)
-		quit(1)
+	V = len(word_idx)
+	first_w = idx_word[0]
+	idx_word[0] = '*dummy*'
+	idx_word[V] = first_w
+	word_idx[first_w] = V
+	word_idx['*dummy*'] = 0
 
-	max_len_en = get_max_len([tr_s,val_s],tokenize = True)
-	max_len_de = get_max_len([tr_t,val_t]) + 1            # for EOS
+	return word_idx, idx_word
 
-	tr_t = open_file(path_prefix + train_target)
-	vocab_de = defaultdict(int)
-	for de in [tr_t]:
-		for line in de:
-			for tok in line.lower().strip().split():
-				vocab_de[tok] +=1
+def prepare_train(path_prefix = '../data/', train_source = 'train.en', train_target = 'train.de', val_source = 'val.en', val_target = 'val.de', TRESHOLD = 1, train_img = 'TRAIN.vgg19.fc7.pkl', val_img = 'VAL.vgg19.fc7.pkl', use_hierarchical = True, repeat = False, suffix = '.all.tokenized.unkified'):
 
-	words = [w for w in vocab_de]
-	vocab_de[UNK] = TRESHOLD + 1
-	vocab_de[EOS] = TRESHOLD + 1
-	for w in words:
-		if vocab_de[w] <= TRESHOLD:
-			del vocab_de[w]
-	word_idx = dict((c, i) for i, c in enumerate(vocab_de))
-	idx_word = dict((i, c) for i, c in enumerate(vocab_de))
-	print >> sys.stderr, "vocabulary size for german is %d" % (len(idx_word))
+	tr_t = open_file(path_prefix + train_target + suffix)
+	val_t = open_file(path_prefix + val_target + suffix)
 
-	X_tr = process_source(path_prefix + train_source, v_map, dim, mini_batch = mini_batch, max_len = max_len_en)
-	Y_tr = process_target(path_prefix + train_target, word_idx, max_len = max_len_de)
-	X_tr_img = process_image(path_prefix + train_img)
+	tr_s = open_file(path_prefix + train_source + suffix)
+	val_s = open_file(path_prefix + val_source + suffix)
 
-	X_val = process_source(path_prefix + val_source, v_map, dim, mini_batch = mini_batch, max_len = max_len_en)
-	Y_val = process_target(path_prefix + val_target, word_idx, max_len = max_len_de)
-	X_val_img = process_image(path_prefix + val_img)
+	max_len_en = get_max_len([tr_s,val_s])
+	max_len_de = get_max_len([tr_t,val_t])
 
-	return X_tr, Y_tr, X_tr_img, X_val, Y_val, X_val_img, word_idx, idx_word
+	word_idx_en, idx_word_en = get_dicts([path_prefix + train_source + suffix, path_prefix + val_source + suffix])
+	word_idx_de, idx_word_de = get_dicts([path_prefix + train_target + suffix, path_prefix + val_target + suffix])
 
+	print >> sys.stderr, "vocabulary size for english %d and for german is %d \n max seq len for english %d and for german is %d" % (len(idx_word_en),len(idx_word_de),max_len_en, max_len_de)
 
-def prepare_test(path_prefix = '../data/', test_source = 'val.en', test_target = 'val.de', wvec = '../embeddings/word2vec.pkl', test_img = 'VAL.vgg19.fc7.pkl', mini_batch = False):
+	X_tr, length_tr = process_source(path_prefix + train_source  + suffix, word_idx_en, max_len = max_len_en)
+	Y_tr = process_target(path_prefix + train_target + suffix, word_idx_de, max_len = max_len_de, use_hierarchical = use_hierarchical)
+	X_tr_img = process_image(path_prefix + train_img, repeat = repeat)
 
-	test_s = open_file(path_prefix + test_source)
-	test_t = open_file(path_prefix + test_target)
+	X_val, length_val = process_source(path_prefix + val_source + suffix, word_idx_en,  max_len = max_len_en)
+	Y_val = process_target(path_prefix + val_target + suffix, word_idx_de, max_len = max_len_de, use_hierarchical = use_hierarchical)
+	X_val_img = process_image(path_prefix + val_img, repeat = repeat)
 
-	try:
-		print >> sys.stderr, "loading word vectors..."
-		v_map = pickle.load(gzip.open(wvec, "rb"))
-		dim = len(list(v_map[UNK]))
-		print >> sys.stderr, "%d dim word vectors are loaded.." % (dim)
-	except:
-		print >> sys.stderr, "word embedding file %s cannot be read." % (wvec)
-		quit(1)
+	dicts = {'word_idx_en' :  word_idx_en, 'idx_word_en' : idx_word_en, 'word_idx_de' :  word_idx_de, 'idx_word_de' : idx_word_de}
 
-	max_len_en = get_max_len([test_s],tokenize = True)
+	if suffix == '.debug':
+		X_tr_img = X_tr_img[:X_tr.shape[0]]
+		X_val_img = X_val_img[:X_val.shape[0]]
 
-	X_test = process_source(path_prefix + test_source, v_map, dim, mini_batch = mini_batch, max_len = max_len_en)
-	X_test_img = process_image(path_prefix + test_img)
+	return X_tr, Y_tr, X_tr_img, X_val, Y_val, X_val_img, dicts , [length_tr, length_val]
 
-	return X_test, test_t, X_test_img
+def prepare_test(word_idx_en, path_prefix = '../data/', test_source = 'val.en', test_target = 'val.de', test_img = 'VAL.vgg19.fc7.pkl', repeat = False, suffix = '.all.tokenized.unkified'):
+
+	test_s = open_file(path_prefix + test_source + suffix)
+	test_t = open_file(path_prefix + test_target + suffix)
+
+	max_len_en = get_max_len([test_s])
+
+	X_test, length_test = process_source(path_prefix + test_source + suffix, word_idx_en, max_len = max_len_en)
+	X_test_img = process_image(path_prefix + test_img, repeat = repeat)
+
+	return X_test, test_t, X_test_img , length_test
